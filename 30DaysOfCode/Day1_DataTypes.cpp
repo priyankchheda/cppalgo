3b8ec2700c75b03cacb6762a48a40f3e448db8aa
x899 <p.chheda29@gmail.com>
2019-05-03 18:36:36 +0530

    add 30daysofcode day 1

---
+++ 30DaysOfCode/Day1_DataTypes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank";
+
+    int ui;
+    double ud;
+    string us;
+
+    cin >> ui;
+    cin >> ud;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin, us);
+
+    cout << i + ui << endl;
+    cout << fixed << setprecision(1) << d + ud << endl;
+    cout << s + us << endl;
+}
