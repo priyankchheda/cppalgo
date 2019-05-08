654eaa1a602aa843ad56b760925c3881d1f40c20
x899 <p.chheda29@gmail.com>
2019-05-08 20:42:56 +0530

    add 30daysofcode day 6

---
+++ 30DaysOfCode/Day06_LetsReview.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int t;
+    std::string str;
+    std::cin >> t;
+    for(int i=0; i < t; i++) {
+        std::cin >> str;
+        std::string even, old;
+        for (int i = 0; i < str.length(); i++) {
+            if (i % 2 == 0) {
+                even += str[i];
+            } else {
+                old += str[i];
+            }
+        }
+        std::cout << even << " " << old << std::endl;
+    }
+}
