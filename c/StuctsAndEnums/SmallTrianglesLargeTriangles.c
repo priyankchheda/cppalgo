66779f58824531c077c875a27b5905e8ff5d0fcf
x899 <p.chheda29@gmail.com>
2019-06-27 17:47:25 +0530

    add c structs and Enums

---
+++ c/StuctsAndEnums/SmallTrianglesLargeTriangles.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct triangle
+{
+    int a;
+    int b;
+    int c;
+};
+
+typedef struct triangle triangle;
+void sort_by_area(triangle* tr, int n) {
+    for (int i = 0; i < n; i++) {
+        int min_index = i;
+        
+    }
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    triangle *tr = malloc(n * sizeof(triangle));
+    for (int i = 0; i < n; i++) {
+        scanf("%d%d%d", &tr[i].a, &tr[i].b, &tr[i].c);
+    }
+    sort_by_area(tr, n);
+    for (int i = 0; i < n; i++) {
+        printf("%d %d %d\n", tr[i].a, tr[i].b, tr[i].c);
+    }
+    return 0;
+} 
