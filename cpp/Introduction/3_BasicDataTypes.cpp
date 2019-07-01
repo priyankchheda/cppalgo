9411db4c4b49b60adf914338c0a698e187961393
x899 <p.chheda29@gmail.com>
2019-07-01 17:45:59 +0530

    cpp basic datatype

---
+++ cpp/Introduction/3_BasicDataTypes.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+int main()
+{
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
+
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%c\n", c);
+    printf("%f\n", d);
+    printf("%lf", e);
+}
