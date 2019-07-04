8d543c77dfe35c4eb57d86b8918adb202caf5bc3
x899 <p.chheda29@gmail.com>
2019-07-04 11:21:35 +0530

    cpp conditional statements

---
+++ cpp/Introduction/4_ConditionalStatements.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    std::string dataSet[] = {
+        "Greater than 9", "one", "two", "three", "four",
+        "five", "six", "seven", "eight", "nine"};
+
+    if (n > 9) {
+        std::cout << dataSet[0];
+    } else {
+        std::cout << dataSet[n];
+    } 
+}
