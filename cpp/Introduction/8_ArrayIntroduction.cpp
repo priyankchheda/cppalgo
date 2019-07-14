fbd3b3cc27e76195eb780e8a83fce560d3e4f859
x899 <p.chheda29@gmail.com>
2019-07-14 11:56:28 +0530

    cpp array

---
+++ cpp/Introduction/8_ArrayIntroduction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        std::cin >> arr[i];
+    }
+    
+    for (int j = n-1; j >= 0; j--) {
+        std::cout << arr[j] << " ";
+    }
+    return 0;
+}
